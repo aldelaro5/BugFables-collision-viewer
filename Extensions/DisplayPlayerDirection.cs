98a56b1687555f02fa5867341ea916fd2a26d0ee
aldelaro5 <aldelaro5@gmail.com>
2021-02-26 22:24:29 -0500

    Format the code

--- Extensions/DisplayPlayerDirection.cs
+++ Extensions/DisplayPlayerDirection.cs
@@ -4,74 +4,83 @@
 
 using UnityEngine;
 
-public class DisplayPlayerDirection : MonoBehaviour {
-	//constants
-	const float KABBU_DASH_DISTANCE = 3.9f; // Is 3.5f but is ajusted to 3.9f following a function call in the original game
+public class DisplayPlayerDirection : MonoBehaviour
+{
+  //constants
+  const float KABBU_DASH_DISTANCE = 3.9f; // Is 3.5f but is ajusted to 3.9f following a function call in the original game
 
-	//Singleton
-	public static DisplayPlayerDirection Instance() { return m_instance; }
-	static DisplayPlayerDirection m_instance;
+  //Singleton
+  public static DisplayPlayerDirection Instance() { return m_instance; }
+  static DisplayPlayerDirection m_instance;
 
-	//Variables
-	Shader m_shaderSpritesDefault;
-	Material m_material;
-	GameObject m_directionAction;
-	LineRenderer m_lineRenderer;
-	KeyCode m_keyCodeEnable = KeyCode.Tab;
+  //Variables
+  Shader m_shaderSpritesDefault;
+  Material m_material;
+  GameObject m_directionAction;
+  LineRenderer m_lineRenderer;
+  KeyCode m_keyCodeEnable = KeyCode.Tab;
 
-    private void Start() {
-        //Initialize
-        if (m_instance == null) {
-            //This is now the singleton instance
-            m_instance = this;
+  private void Start()
+  {
+    //Initialize
+    if (m_instance == null)
+    {
+      //This is now the singleton instance
+      m_instance = this;
 
-			//Shader
-			m_shaderSpritesDefault = Shader.Find("Sprites/Default");
+      //Shader
+      m_shaderSpritesDefault = Shader.Find("Sprites/Default");
 
-			//Materials
-			if (m_shaderSpritesDefault != null) {
-				m_material = new Material(m_shaderSpritesDefault);
-				m_material.color = Color.yellow;
-			}
+      //Materials
+      if (m_shaderSpritesDefault != null)
+      {
+        m_material = new Material(m_shaderSpritesDefault);
+        m_material.color = Color.yellow;
+      }
 
-			//GameObejct
-			m_directionAction = new GameObject("MOD_DirectionAction");
-			m_directionAction.transform.parent = this.transform;
-			m_lineRenderer = m_directionAction.AddComponent<LineRenderer>();
-			m_lineRenderer.enabled = true;
-			m_lineRenderer.alignment = LineAlignment.TransformZ;
-			m_lineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-			m_lineRenderer.receiveShadows = false;
-			m_lineRenderer.allowOcclusionWhenDynamic = false;
-			m_lineRenderer.useWorldSpace = true;
-			m_lineRenderer.startWidth = 0.1f;
-			m_lineRenderer.endWidth = 0.1f;
-			m_lineRenderer.material = m_material;
-		}
-        else if (this != m_instance) {
-            Destroy(this);
-        }
+      //GameObejct
+      m_directionAction = new GameObject("MOD_DirectionAction");
+      m_directionAction.transform.parent = this.transform;
+      m_lineRenderer = m_directionAction.AddComponent<LineRenderer>();
+      m_lineRenderer.enabled = true;
+      m_lineRenderer.alignment = LineAlignment.TransformZ;
+      m_lineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+      m_lineRenderer.receiveShadows = false;
+      m_lineRenderer.allowOcclusionWhenDynamic = false;
+      m_lineRenderer.useWorldSpace = true;
+      m_lineRenderer.startWidth = 0.1f;
+      m_lineRenderer.endWidth = 0.1f;
+      m_lineRenderer.material = m_material;
     }
+    else if (this != m_instance)
+    {
+      Destroy(this);
+    }
+  }
 
-    //Update
-    void Update() {
-		///Check if initilization was fine
-		if (m_lineRenderer != null) {
-			PlayerControl t_player = MainManager.player;
-			//Check if find the player
-			if (t_player == null) {
-				//Reset the line positions
-				m_lineRenderer.SetPositions(new Vector3[2] { new Vector3(), new Vector3() });
-			}
-			else {
-				//Direction you are facing
-				Vector3 t_fromPos = t_player.entity.transform.position + new Vector3(0f, 0.5f, 0f);
-				Vector3 t_toPos = t_fromPos + new Vector3(t_player.lastdelta.x, t_player.lastdelta.y, t_player.lastdelta.z).normalized * KABBU_DASH_DISTANCE;
-				m_lineRenderer.SetPositions(new Vector3[] { t_fromPos, t_toPos });
-			}
+  //Update
+  void Update()
+  {
+    ///Check if initilization was fine
+    if (m_lineRenderer != null)
+    {
+      PlayerControl t_player = MainManager.player;
+      //Check if find the player
+      if (t_player == null)
+      {
+        //Reset the line positions
+        m_lineRenderer.SetPositions(new Vector3[2] { new Vector3(), new Vector3() });
+      }
+      else
+      {
+        //Direction you are facing
+        Vector3 t_fromPos = t_player.entity.transform.position + new Vector3(0f, 0.5f, 0f);
+        Vector3 t_toPos = t_fromPos + new Vector3(t_player.lastdelta.x, t_player.lastdelta.y, t_player.lastdelta.z).normalized * KABBU_DASH_DISTANCE;
+        m_lineRenderer.SetPositions(new Vector3[] { t_fromPos, t_toPos });
+      }
 
-            if (Input.GetKeyDown(m_keyCodeEnable))
-				m_lineRenderer.enabled = !m_lineRenderer.enabled; 
-		}
-	}
+      if (Input.GetKeyDown(m_keyCodeEnable))
+        m_lineRenderer.enabled = !m_lineRenderer.enabled;
+    }
+  }
 }
