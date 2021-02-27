32de207498449bed863a15c946cd643be613eb1f
aldelaro5 <aldelaro5@gmail.com>
2021-02-26 22:26:15 -0500

    VERSION BUMP

--- CollisionViewerPlugin.cs
+++ CollisionViewerPlugin.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 namespace CollisionViewer
 {
-  [BepInPlugin("com.aldelaro5.BugFables.plugins.CollisionViewer", "Collision Viewer", "2.0.0")]
+  [BepInPlugin("com.aldelaro5.BugFables.plugins.CollisionViewer", "Collision Viewer", "2.0.1")]
   [BepInProcess("Bug Fables.exe")]
   public class CollisionViewerPlugin : BaseUnityPlugin
   {
